36688e21b2f44eb5864de3c72268cf93321f1ad6
Bravo Kiptoo <kiptoob@familybank.co.ke>
2023-03-04 11:03:11 +0300

    Test for weather api

--- api/tests/Feature/ExampleTest.php
+++ api/tests/Feature/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use GuzzleHttp\Client;
 
 class ExampleTest extends TestCase
 {
@@ -27,5 +28,15 @@ class ExampleTest extends TestCase
         User::factory(20)->create();
 
         $this->assertEquals(20, User::all()->count());
+    }
+
+    public function test_weather_api_returns_successful_response(): void
+    {
+        $api_key = env("TOMORROW_API_KEY");
+        $url = "https://api.tomorrow.io/v4/weather/forecast?location=35.31982200,176.26737000&timesteps=1h&units=metric&apikey={$api_key}";
+        $client = new Client(['verify' => false]);
+        $response = $client->get($url);
+        
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
